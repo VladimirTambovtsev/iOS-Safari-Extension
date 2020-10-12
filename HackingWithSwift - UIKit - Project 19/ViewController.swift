ec62544e10c502c2b98c26e9728fab86944a7282
VladimirTambovtsev <tambovcev99@mail.ru>
2020-10-12 19:00:40 +0300

    Initial Commit

---
+++ HackingWithSwift - UIKit - Project 19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackingWithSwift - UIKit - Project 19
+//
+//  Created by Vladimir on 12.10.2020.
+//  Copyright © 2020 Vladimir Tambovtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
